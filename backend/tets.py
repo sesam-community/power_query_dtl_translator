d25013f110b67fe4e1eb03820d904ae3a3232bdc
erik-leven <erik.leven@sesam.io>
2019-08-21 11:01:25 +0200

    added transforms

--- backend/tets.py
+++ backend/tets.py
@@ -1,17 +1,45 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, CORS, cross_origin
 import requests
 import json
+import sys
 
 app           = Flask(__name__)
+CORS(app, resources={r”/“: {“origins”: “”}},
+headers={‘Access-Control-Request-Headers’, ‘Content-Type’, ‘Access-Control-Allow-Origin’})
 
+dtl_prefix = '"transform": {"type": "dtl","rules":{"default": [["copy", "*"],'
+dtl_postfix = ']}}'
+text_example = 'let Kilde = Csv.Documents(File.Contents("C:Userserik1DownloadsSampleCSVFile_2kb.csv"),[Delimiter=",", Columns=10, Encoding=1252, QuoteStyle=QuoteStyle.None]), #"Fjernede kolonner" = Table.RemoveColumns(Kilde, {"Column1"}), #"Text med store bokstaver" = Table.TransformColumns(#"Fjernede kolonner", {{"Column2", Text.Upper, type text}}) in #"Tetx med store bokstaver"'
+words = text_example.split()
 
-text_example = 'let Kilde = Csv.Documents(File.Contents("C:\Users\erik1\Doownloads\SampleCSVFile_2kb.csv"),[Delimiter=",", Columns=10, Encoding=1252, QuoteStyle=QuoteStyle.None]), #"Fjernede kolonner" = Table.RemoveColumns(Kilde, {"Columns1"}), '
+def transform(query):
+	dtl_code = str()
+	words = query.split()
+	for i, word in enumerate(words):
+		if word[:5] == "Table":
+			command = word.split('.')[1].split('(')[0]
+			if command == "RemoveColumns":
+				property = words[i+1].split('"')[1]
+				dtl_code += '["remove", %s],' %property
+			elif command == "TransformColumns":
+				property = words[i+2].split('"')[1]
+				dtl_code += '["remove", "%s"],' %property
+				dtl_code += '["add", %s, ["upper", _S.%s]],' %(property, property)
+	return dtl_code[:-2]
 
+@app.route('/query', methods=['POST'])
+@cross_origin()
+def query():
+	reponse = request.json()
+	global query
+	query = str(response["pbiInput"])
 
-@app.route('/transform', methods=['GET', 'POST'])
-def transform(text):
+@app.route('/dtl_transform', methods=['GET'])
+def dtl_transform():
+	global query
+	dtl_code = transform(query)
 
-
+	return jsonify(dtl_code.json())
 if __name__ == '__main__':
 
     # This is used when running locally. Gunicorn is used to run the
